72c908c00cf4724514a96e2f2d649f601e274a87
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-09-30 04:06:44 -0300

    Adicionado comentários referentes aos próximos passos do AlunoDao

---
+++ CP-02-DDD/src/main/java/br/com/fiap/cp2/dao/AlunoDao.java
@@ -0,0 +1,27 @@
+package br.com.fiap.cp2.dao;
+
+import java.sql.Connection;
+
+import br.com.fiap.cp2.interfaces.Dao;
+
+public class AlunoDao implements Dao{
+	//Atributos
+	private Connection conn;
+	
+	//Construtor
+	public AlunoDao(Connection conn) {
+		this.conn = conn;
+	}//AlunoDao
+	
+	//Parse
+	
+	//Cadastrar
+	
+	//Listar
+	
+	//PesquisarPorCodigo
+	
+	//Deletar
+	
+	//Atualizar
+}//CLASS
