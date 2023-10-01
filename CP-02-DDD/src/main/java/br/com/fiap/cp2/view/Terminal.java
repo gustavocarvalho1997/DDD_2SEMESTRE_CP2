8f5d78ee18190042340c748dea109de0c4c6ab2e
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-09-30 22:16:04 -0300

    Feito o corpo do menu que irá simular as operações na view e implementada a opção 1

--- CP-02-DDD/src/main/java/br/com/fiap/cp2/view/Terminal.java
+++ CP-02-DDD/src/main/java/br/com/fiap/cp2/view/Terminal.java
@@ -18,10 +18,30 @@ public class Terminal {
 			
 			while(true) {
 				JOptionPane.showConfirmDialog(null, "Bem vindo(a), a seguir serão mostradas as opções de interação com o banco de dados criado!");
-				
-			}
-			
-			
+				int opcao = Integer.parseInt(JOptionPane.showInputDialog("Digite o número para selecionar sua opção:\n1 - Cadastrar um aluno\n2 - Listar os alunos\n3 - Pesquisar um aluno pelo seu código\n4 - Deletar um aluno do banco pelo seu respectivo código\n5 - Atualizar as informações de um aluno\n6 - Sair"));
+				if (opcao == 1) {
+					String primeiroNome = JOptionPane.showInputDialog("Informe o primeiro nome do aluno: ");
+					String sobrenome = JOptionPane.showInputDialog("Informe o sobrenome do aluno:");
+					String curso = JOptionPane.showInputDialog("Informe o curso do aluno:");
+					String instituicao = JOptionPane.showInputDialog("Informe a instituição de ensino:");
+					int anoDeInicio = Integer.parseInt(JOptionPane.showInputDialog("Informe o ano de início desse aluno:\n Ex: 2023"));
+					Aluno aluno = new Aluno(primeiroNome, sobrenome, curso, instituicao, anoDeInicio);
+					dao.cadastrar(aluno);
+					System.out.println("Cadastro efetuado com sucesso!");
+				} else if (opcao == 2) {
+					dao.listar();
+				} else if (opcao == 3 ) {
+					dao.pesquisarPorCodigo(1);
+				} else if (opcao == 4) {
+					dao.deletar(2);
+				} else if (opcao == 5) {
+					dao.atualizar(null);
+				} else if (opcao == 6) {
+					break;
+				} else {
+					System.out.println("Opção selecionada não reconhecida!");
+				}
+			}//While
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
